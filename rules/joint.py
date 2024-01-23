47c20cee25cc103f17fbb724f55a5caa0b9eff9a
blitzapurv <apurvsingh97@gmail.com>
2024-01-23 23:55:50 +0530

    optimisation tests

--- rules/joint.py
+++ rules/joint.py
@@ -11,31 +11,29 @@ from ti_tools.moving_average_convergence_divergence import (moving_average_conve
 
 
 class MACDandRSI(Strategy):
-    params = {
-        # define MACD, signal lengths
-        "fast_length": 12,
-        "slow_length": 26,
-        "signal_length": 9,
-        # define RSI bounds and length
-        "rsi_window": 14,
-        "upper_bound": 70,
-        "lower_bound": 30,
-    }
+    # define MACD, signal lengths
+    fast_length = 12
+    slow_length = 26
+    signal_length = 9
+    # define RSI bounds and length
+    rsi_window = 14
+    upper_bound = 70
+    lower_bound = 30
 
-    _props = f"{params['fast_length']}_{params['slow_length']}_{params['signal_length']}"
+    _props = f"{fast_length}_{slow_length}_{signal_length}"
     _temp_buying_posi = np.inf
 
     def init(self):
-        print(self._props)
-        self.macd = self.I(moving_average_convergence_divergence, self.data.Close, short_period=self.params['fast_length'], long_period=self.params['slow_length'])
-        self.signal = self.I(signal_line, self.macd, period=self.params['signal_length'])
-        self.rsi = self.I(rsi, pd.Series(self.data.Close), period=self.params['rsi_window'])
+        # print(self._props)
+        self.macd = self.I(moving_average_convergence_divergence, self.data.Close, short_period=self.fast_length, long_period=self.slow_length)
+        self.signal = self.I(signal_line, self.macd, period=self.signal_length)
+        self.rsi = self.I(rsi, pd.Series(self.data.Close), period=self.rsi_window)
     # sell everything if signal moves above macd and buy if it's vica-versa
     def next(self):
-        if crossover(self.macd, self.signal) and (self.rsi[-1]<self.params['lower_bound']):
+        if crossover(self.macd, self.signal) and (self.rsi[-1]<self.lower_bound):
             self.position.close()
             self.buy()
-            print("buying position :", self.data.Close[-1])
+            # print("buying position :", self.data.Close[-1])
             self._temp_buying_posi = self.data.Close[-1]
-        elif crossover(self.signal, self.macd) and (self.rsi[-1]>self.params['upper_bound']):
+        elif crossover(self.signal, self.macd) and (self.rsi[-1]>self.upper_bound):
             self.position.close()
